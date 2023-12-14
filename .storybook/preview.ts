660c3dade2066fce27a31efd8e87f20aef1816d8
İrfan TARI <irfantari998@gmail.com>
2023-12-15 01:03:58 +0300

    chore: install storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,28 @@
+import type { Preview } from '@storybook/react'
+import '../tailwind.css'
+
+import { withThemeByClassName } from '@storybook/addon-themes'
+
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }) as never,
+  ],
+}
+
+export default preview
